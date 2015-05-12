2833fb361f4d8cfa2150dc67b239d341058a9908
kingspp <kingspprathyush@gmail.com>
2015-05-12 09:18:17 +0530

    Added HTML Template

--- PiGen.py
+++ PiGen.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main():
+  
+   return render_template('main.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
