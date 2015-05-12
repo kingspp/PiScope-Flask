0e7b1897e82d3815a000ba3310c02eac9476defc
kingspp <kingspprathyush@gmail.com>
2015-05-12 15:15:41 +0000

    Added Footer with versioning

--- static/js/creative.js
+++ static/js/creative.js
@@ -13,6 +13,8 @@
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
+		
+		 
         event.preventDefault();
     });
 
@@ -21,6 +23,8 @@
         target: '.navbar-fixed-top',
         offset: 51
     })
+	
+	
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -39,6 +43,7 @@
     $('#mainNav').affix({
         offset: {
             top: 100
+			
         }
     })
 
