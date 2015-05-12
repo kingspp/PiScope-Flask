9e175c29b5e94497cd2b1fbcf7eb48fd1e88ebce
kingspp <kingspprathyush@gmail.com>
2015-05-12 13:21:00 +0530

    Added WPA_PSK Authorization

--- PiGen.py
+++ PiGen.py
@@ -1,11 +1,28 @@
 from flask import Flask
-from flask import Flask, render_template, request
+from flask.ext.httpauth import HTTPBasicAuth
+from flask import Flask, flash, render_template, request
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+app.secret_key = 'some_secret'
+
+users = {
+    "admin": "1234",
+    "susan": "bye"
+}
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
 
 @app.route("/")
+@auth.login_required
 def main():
-  
+   user =  auth.username()
+   #flash('Welcome %s', % user)
+   
    return render_template('main.html')
 
 if __name__ == "__main__":
