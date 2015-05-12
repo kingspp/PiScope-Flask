a97698896a37a47b79fad33e3e024c641789238c
kingspp <kingspprathyush@gmail.com>
2015-05-12 15:45:28 +0530

    Added display of Tuning word

--- PiGen.py
+++ PiGen.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask.ext.httpauth import HTTPBasicAuth
 from flask import Flask, flash, render_template, request, redirect
 from flask import Markup
+import math
+from lib2to3.fixer_util import String
 
 
 ip = '192.168.0.4'
-
+defClock = 125000000
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -30,7 +32,7 @@ def main():
    return render_template('main.html')
 @app.route("/my_form_post", methods=['POST'])
 def my_form_post():
-    name=request.form['usr'] 
+    value=request.form['usr'] 
     hz=request.form['val']
     if hz=='hz':
         hz='Hertz'    
@@ -38,13 +40,18 @@ def my_form_post():
         hz='Kilo Hertz'    
     elif hz=='mhz':
         hz='Mega Hertz'
-        
     
-    message = Markup("<h3>Tuning Word: "+hz+"</h3>")
+    message = Markup("<h3>Value: "+value+" "+hz+"</h3>")    
+    value = tuningWord(float(value))   
+    value = str(value)
+    print value  
+    message+= Markup("<h3>Tuning Word: "+value+"</h3>")
     flash(message) 
     return redirect("http://"+ip+"#pigen")
 
-
+def tuningWord(value):
+    tw= value* (math.pow(2, 32) / defClock)
+    return tw
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
